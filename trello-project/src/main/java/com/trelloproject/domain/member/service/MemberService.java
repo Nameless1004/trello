75f36d5e78928507d58d8593d848e01a0b9cf949
KJG04 <freedom7113@gmail.com>
2024-10-14 17:24:01 +0900

    update: 멤버 역할 변경 서비스 메서드 추가

--- trello-project/src/main/java/com/trelloproject/domain/member/service/MemberService.java
+++ trello-project/src/main/java/com/trelloproject/domain/member/service/MemberService.java
@@ -1,7 +1,21 @@
 package com.trelloproject.domain.member.service;
 
+import com.trelloproject.common.enums.MemberRole;
+import com.trelloproject.domain.member.entity.Member;
+import com.trelloproject.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member updateMemberRole(Long memberId, MemberRole memberRole) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("해당하는 memberId의 멤버가 존재하지 않습니다."));
+        member.setRole(memberRole);
+        return memberRepository.save(member);
+    }
 }

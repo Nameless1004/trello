6ca4831052d5245092e4abdb827d6271c26dba18
KJG04 <freedom7113@gmail.com>
2024-10-15 10:27:58 +0900

    update: 워크스페이스 생성 기능 구현

--- trello-project/src/main/java/com/trelloproject/domain/workspace/controller/WorkspaceController.java
+++ trello-project/src/main/java/com/trelloproject/domain/workspace/controller/WorkspaceController.java
@@ -4,13 +4,12 @@ import com.trelloproject.common.dto.ResponseDto;
 import com.trelloproject.common.enums.UserRole;
 import com.trelloproject.domain.workspace.dto.WorkspaceRequest;
 import com.trelloproject.domain.workspace.dto.WorkspaceResponse;
+import com.trelloproject.domain.workspace.entity.Workspace;
 import com.trelloproject.domain.workspace.service.WorkspaceService;
-import com.trelloproject.security.AuthUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +21,8 @@ public class WorkspaceController {
 
     @PostMapping("/workspaces")
     @Secured(UserRole.Authority.ADMIN)
-    public ResponseDto<WorkspaceResponse.CreateWorkspace> createWorkspace(@Valid @RequestBody WorkspaceRequest.CreateWorkspace createWorkspaceRequestDto, @AuthenticationPrincipal AuthUser authUser) {
-        return ResponseDto.of(HttpStatus.OK);
+    public ResponseDto<WorkspaceResponse.CreateWorkspace> createWorkspace(@Valid @RequestBody WorkspaceRequest.CreateWorkspace createWorkspaceRequestDto) {
+        Workspace workspace = workspaceService.createWorkspace(createWorkspaceRequestDto.name(), createWorkspaceRequestDto.description());
+        return ResponseDto.of(HttpStatus.CREATED, "워크스페이스가 생성되었습니다.", new WorkspaceResponse.CreateWorkspace(workspace.getId(), workspace.getName(), workspace.getDescription()));
     }
 }

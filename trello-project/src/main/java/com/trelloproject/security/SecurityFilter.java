a7ed463f4789e94d7b12a0cbadebde0f81735f04
Nameless1004 <jjho1029@gmail.com>
2024-10-17 15:22:19 +0900

    fix: 로그아웃 안되는 부분 수정

--- trello-project/src/main/java/com/trelloproject/security/SecurityFilter.java
+++ trello-project/src/main/java/com/trelloproject/security/SecurityFilter.java
@@ -28,7 +28,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
         log.info("Request URI: {}", requestURI);
-        if(!requestURI.startsWith("/auth/signout") && requestURI.startsWith("/auth/")) {
+        if(!requestURI.startsWith("/auth/logout") && requestURI.startsWith("/auth/")) {
             filterChain.doFilter(request, response);
             return;
         }
